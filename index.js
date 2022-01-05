94c0b3be7cdb48e1a852aa31366b53d221f35fbb
Anish Gupta <anish-gupta@outlook.com>
2022-01-05 20:33:45 +0000

    2fa

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 /* tslint:disable:no-console */
 require('dotenv').config()
+const inquirer = require('inquirer');
 const { IgApiClient } = require('instagram-private-api');
+
 
 const ig = new IgApiClient();
 
 ig.state.generateDevice(process.env.IG_USERNAME);
 
 (async () => {
+  try {
   await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-
+  }
+  catch (err) {
+    const {username, totp_two_factor_on, two_factor_identifier} = err.response.body.two_factor_info;
+      // decide which method to use
+      const verificationMethod = totp_two_factor_on ? '0' : '1'; // default to 1 for SMS
+      // At this point a code should have been sent
+      // Get the code
+      const { code } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'code',
+          message: `Enter code received via ${verificationMethod === '1' ? 'SMS' : 'TOTP'}`,
+        },
+      ]);
+      await ig.account.twoFactorLogin({
+        username,
+        verificationCode: code,
+        twoFactorIdentifier: two_factor_identifier,
+        verificationMethod, // '1' = SMS (default), '0' = TOTP (google auth for example)
+        trustThisDevice: '1', // Can be omitted as '1' is used by default
+      });
+  }
   const followersFeed = ig.feed.accountFollowers(ig.state.cookieUserId);
   const followingFeed = ig.feed.accountFollowing(ig.state.cookieUserId);
 
